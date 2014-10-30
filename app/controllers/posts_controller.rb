0bed7a56a769240c9f6f32dbd436172729b8a999
Iyad Horani <i3d@ironic3d.com>
2014-10-30 01:40:50 +0000

    Add edit and update actions and style flash messages

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -14,14 +14,26 @@ class PostsController < ApplicationController
   def create
     @post = Post.new(params.require(:post).permit(:title, :body))
     if @post.save
-      flash[:notice] = "Post was Saved"
+      flash[:success] = "Post was Saved"
       redirect_to @post
     else
-      flasj[:error] = "There was an error saving the post. Please try again"
+      flash[:error] = "There was an error saving the post. Please try again"
       render :new
     end
   end
 
   def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update_attributes(params.require(:post).permit(:title, :body))
+      flash[:success] = "Post was updated successfully"
+      redirect_to @post
+    else
+      flash[:error] = "There was an error updating the post. Please try again"
+      render :edit
+    end
   end
 end
